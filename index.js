7a9e2b21bbfd3389fe945bc5f96ddf6dde6692ed
tsertic <tsertic5@gmail.com>
2023-09-30 22:44:48 +0200

    basic logic to take screenshot

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const express = require("express");
-const app = express();
+const puppeteer = require("puppeteer");
+const getScreen = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.instagram.com/artemisia96/?hl=en", {
+    waitUntil: "networkidle0",
+  });
+  const [button] = await page.$x("//button[contains(., 'Allow all')]");
+  if (button) {
+    await button.click();
+    await page.waitForNetworkIdle({ idleTime: 1000 });
+    const dateNow = new Date();
+    await page.screenshot({
+      path: `screenshot/${Math.floor(Math.random() * 1000000000000)}.png`,
+      fullPage: true,
+    });
+  }
+  console.log("u kodu");
+  await browser.close();
+};
 
-const PORT = process.env.PORT || 5555;
-
-app.get("/", (req, res) => {
-  res.send("HEllo");
-});
-
-app.listen(PORT, () => {
-  return console.log("Server up and running on port: ", PORT);
-});
+setInterval(() => {
+  console.log("Running");
+  getScreen();
+}, 100000000);
